71f1b9f30335208a6906a316238ee3e1c2f2e48f
m-bernardin <mjrb4mjrb@gmail.com>
2026-03-02 16:13:18 -0500

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,8 @@
+// 2- WAP to create the class Person having its parameterized constructor, and 2 attributes as name and age.
+//  Create the class Student having its attribute as student_id. Class Student inherits class Person.
+//  Create a driver class to run the program and provide the user with the given menu. 
+// • User can unlimitedly add Student any time; and therefore the existing info gets updated accordingly.
+// • Ensure, the code always show the last updated info for choice 2 and 3 
+public class Driver {
+    
+}
---
+++ Words.java
@@ -0,0 +1,7 @@
+//1- Using overloading concept, WAP to create a class “Words” having some methods with the same name.
+//All methods accept string type values as their parameters. Provide the user with a menu as under, 
+//and based on the user’s choice input appropriate data and do what she/he has asked for.
+//When comparing, ignore repeated characters.
+public class Words{
+    
+}
